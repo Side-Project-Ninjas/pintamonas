77fe77b24abf083f16777b67ff072fcb67845619
Diego Rodríguez <diego@rodribus.com>
2016-04-23 19:32:21 +0200

    [ FIX ] Ajustes de rutas en el servidor

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -4,10 +4,11 @@
 'use strict';
 
 var express = require('express');
+var path = require('path');
 var config = require('./environment');
 
 // Set the express config up
 // =========================
 module.exports = function(app) {
-    app.use(express.static(__dirname + config.publicPath));
+    app.use(express.static(path.resolve(config.publicPath)));
 };
