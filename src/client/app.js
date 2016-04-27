15a66d63a98190debfb206995024069b2d9cb435
Sergio Serrano <seserme@gmail.com>
2016-04-27 18:14:29 +0200

    [ FEAT ] Fuentes y sus llamadas en el scss [ FEAT ] Modificacion de plantillas

--- src/client/app.js
+++ src/client/app.js
@@ -1,22 +1,30 @@
-'use strict';
+(function(){
+  'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('pintamonas', [
-  'ngRoute',
-  'pintamonas.login',
-  'pintamonas.painting',
-  'pintamonas.blackboard'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/login'});
+  // Declare app level module which depends on views, and components
+  angular.module('pintamonas', [
+    'ngRoute',
+    'pintamonas.login',
+    'pintamonas.painting',
+    'pintamonas.blackboard'
+  ]).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/'});
 
-  $routeProvider.when('/login', {
-    templateUrl: 'views/login.html',
-    controller: 'LoginCtrl'
-  });
+    $routeProvider.when('/', {
+      templateUrl: 'views/landing.html',
+      controller: 'LandingCtrl'
+    });
 
-  $routeProvider.when('/blackboard', {
-    templateUrl: 'views/blackboard.html',
-    controller: 'blackboardCtrl'
-  });
-}]);
+    $routeProvider.when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl'
+    });
+
+    $routeProvider.when('/blackboard', {
+      templateUrl: '../views/blackboard.html',
+      controller: 'blackboardCtrl'
+    });
+  }]);
+
+})();
