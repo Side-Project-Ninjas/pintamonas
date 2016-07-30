885cebbf015e4ed70837d959fed7e2a7fd061238
Diego Rodríguez <diego@rodribus.com>
2016-07-30 16:31:10 +0200

    fix(gulp): modifica las tareas para levantar browserSync solo como proxy y obliga a levantar el servidor por separado

--- gulpfile.js
+++ gulpfile.js
@@ -15,63 +15,68 @@ var vendor = require("gulp-concat-vendor");
 var debug = require("gulp-debug");
 
 var config = {
-    server: {
-        url: "localhost",
-        port: 3000
+  server: {
+    url: "localhost",
+    // port: 3000,
+    proxy: 8080
+  },
+  paths: {
+    server: "./src/server/index.js",
+    styles: {
+      src: ["./src/client/resources/styles/**/*.scss"],
+      public: "./public/resources/css/",
+      map: "./"
     },
-    paths: {
-        server: "./src/server/index.js",
-        styles: {
-            src: ["./src/client/resources/styles/**/*.scss"],
-            public: "./public/resources/css/",
-            map: "./"
-        },
-        scripts: {
-            src: ["./src/client/**/*.js", "!./src/client/vendor/**/*.js"],
-            public: "./public/resources/js/",
-            map: "./"
-        },
-        templates: {
-            src: ["./src/client/**/*.jade"],
-            public: "./public/"
-        },
-        vendor: {
-            src: ["./src/client/vendor/*"],
-            public: "./public/resources/vendor"
-        }
+    scripts: {
+      src: ["./src/client/**/*.js", "!./src/client/vendor/**/*.js"],
+      public: "./public/resources/js/",
+      map: "./"
+    },
+    templates: {
+      src: ["./src/client/**/*.jade"],
+      public: "./public/"
+    },
+    vendor: {
+      src: ["./src/client/vendor/*"],
+      public: "./public/resources/vendor"
     }
+  }
 };
 
 
 gulp.task("serve", "Sirve la aplicación", ["compile", "nodemon"], function() {
 
-    browserSync.init({
-        proxy: config.server.url + ":" + config.server.port
-    });
+  browserSync.init({
+    proxy: {
+      target: "http://localhost:3000",
+      ws: true
+    },
+    port: config.server.proxy
+  });
 
-    gulp.watch(config.paths.templates.src, ["html"]);
-    gulp.watch(config.paths.styles.src, ["css"]);
-    gulp.watch(config.paths.scripts.src, ["js"]);
-    gulp.watch(config.paths.vendor.src, ["vendor"]);
+  gulp.watch(config.paths.templates.src, ["html"]);
+  gulp.watch(config.paths.styles.src, ["css"]);
+  gulp.watch(config.paths.scripts.src, ["js"]);
+  gulp.watch(config.paths.vendor.src, ["vendor"]);
 });
 
 gulp.task("compile", "Compila los ficheros HTML, CSS y JS", ["html", "css", "js", "vendor"]);
 
 gulp.task("nodemon", "Inicia el servidor y lo reinicia si se producen cambios", function() {
-    nodemon({
-        script: config.paths.server,
-        debug: true,
-        watch: ".\\src\\server\\",
-        env: {
-            "NODE_ENV": "development"
-        }
-    });
+  // nodemon({
+  //   script: config.paths.server,
+  //   debug: true,
+  //   watch: ".\\src\\server\\",
+  //   env: {
+  //     "NODE_ENV": "development"
+  //   }
+  // });
 });
 
 gulp.task("html", "Compila HTML con los ficheros JADE", function() {
-    return gulp.src(config.paths.templates.src)
+  return gulp.src(config.paths.templates.src)
     .pipe(debug({
-        title: "HTML"
+      title: "HTML"
     }))
     .pipe(jade())
     .pipe(gulp.dest(config.paths.templates.public))
@@ -79,44 +84,44 @@ gulp.task("html", "Compila HTML con los ficheros JADE", function() {
 });
 
 gulp.task("css", "Genera app.css con los ficheros SCSS", function() {
-    return gulp.src(config.paths.styles.src)
-        .pipe(debug({
-            title: "CSS"
-        }))
-        .pipe(concat("app.css"))
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: "compressed"
-        }).on("error", sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write(config.paths.styles.map))
-        .pipe(gulp.dest(config.paths.styles.public))
-        .pipe(browserSync.stream());
-        //.pipe(browserSync.stream());
+  return gulp.src(config.paths.styles.src)
+    .pipe(debug({
+      title: "CSS"
+    }))
+    .pipe(concat("app.css"))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: "compressed"
+    }).on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(config.paths.styles.map))
+    .pipe(gulp.dest(config.paths.styles.public))
+    .pipe(browserSync.stream());
+  //.pipe(browserSync.stream());
 });
 
 gulp.task("js", "Genera app.js con los ficheros JS", function() {
-    return gulp.src(config.paths.scripts.src)
-        .pipe(debug({
-            title: "JS"
-        }))
-        .pipe(ngAnnotate())
-        .pipe(eslint())
-        .pipe(eslint.format())
-        //.pipe(eslint.failAfterError())
-        .pipe(concat("app.js"))
-        .pipe(sourcemaps.write(config.paths.scripts.map))
-        //.pipe(browserSync.reload({ stream: true }))
-        .pipe(gulp.dest(config.paths.scripts.public))
-        .pipe(browserSync.stream());
+  return gulp.src(config.paths.scripts.src)
+    .pipe(debug({
+      title: "JS"
+    }))
+    .pipe(ngAnnotate())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    //.pipe(eslint.failAfterError())
+    .pipe(concat("app.js"))
+    .pipe(sourcemaps.write(config.paths.scripts.map))
+    //.pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest(config.paths.scripts.public))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("vendor", "Genera un vendor.js a partir de las dependencias de bower", function() {
-    gulp.src(config.paths.vendor.src)
-        .pipe(debug({
-            title: "VENDOR"
-        }))
-        .pipe(vendor("vendor.js"))
-        .pipe(gulp.dest(config.paths.vendor.public))
-        .pipe(browserSync.stream());
+  gulp.src(config.paths.vendor.src)
+    .pipe(debug({
+      title: "VENDOR"
+    }))
+    .pipe(vendor("vendor.js"))
+    .pipe(gulp.dest(config.paths.vendor.public))
+    .pipe(browserSync.stream());
 });
