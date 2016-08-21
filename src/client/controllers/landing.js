ba07680f9bcca88728612f1edac370b17801ab35
Diego Rodríguez <diego@rodribus.com>
2016-08-21 19:12:29 +0200

    feat(landing): ajusta el controlador para usar spnHttp

--- src/client/controllers/landing.js
+++ src/client/controllers/landing.js
@@ -4,22 +4,21 @@ angular.module('pintamonas.landing', ['ngRoute'])
   .controller('LandingCtrl', landingCtrl);
 
 /** @ngInject */
-function landingCtrl($scope, $http, $location) {
+function landingCtrl($scope, $http, $location, spnHttp) {
   if (document.cookie.indexOf('pintamonas.user') !== -1) {
-    $location.path( "/hub" );
+    $location.path("/hub");
   }
+
   var loginButton = angular.element('.login-button');
+
   loginButton.click(function() {
-    $http({
-      method: 'POST',
-      url: '/api/name',
-      data: {
-        name: angular.element('#username').val()
-      }
-    }).then(function successCallback(response) {
+    var payload = {
+      name: angular.element('#username').val()
+    };
+    spnHttp.httpAlias('name', payload).then(function successCallback(response) {
       if (response.data.status === 'OK') {
-        document.cookie = "pintamonas.user="+response.data.name;
-        $location.path( "/hub" );
+        // document.cookie = "pintamonas.user=" + response.data.name;
+        $location.path("/hub");
       }
     }, function errorCallback(response) {
       console.log("error", response);
