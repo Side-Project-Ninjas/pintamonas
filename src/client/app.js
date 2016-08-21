e1b2991f5140a2e3037c64fff411d468a20f7259
Diego Rodríguez <diego@rodribus.com>
2016-08-21 19:10:50 +0200

    refactor(app): utiliza el modulo spn

--- src/client/app.js
+++ src/client/app.js
@@ -1,30 +1,33 @@
-(function(){
+(function() {
   'use strict';
 
   // Declare app level module which depends on views, and components
   angular.module('pintamonas', [
-    'ngRoute',
-    'pintamonas.landing',
-    'pintamonas.hub',
-    'pintamonas.room'
-  ]).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/'});
+      'ngRoute',
+      'spn',
+      'pintamonas.landing',
+      'pintamonas.hub',
+      'pintamonas.room'
+    ])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.otherwise({
+        redirectTo: '/'
+      });
 
-    $routeProvider.when('/', {
-      templateUrl: 'views/landing.html',
-      controller: 'LandingCtrl'
-    });
+      $routeProvider.when('/', {
+        templateUrl: 'views/landing.html',
+        controller: 'LandingCtrl'
+      });
 
-    $routeProvider.when('/room/:roomName', {
-      templateUrl: '../views/room.html',
-      controller: 'RoomCtrl'
-    });
+      $routeProvider.when('/room/:roomName', {
+        templateUrl: '../views/room.html',
+        controller: 'RoomCtrl'
+      });
 
-    $routeProvider.when('/hub', {
-      templateUrl: '../views/hub.html',
-      controller: 'HubCtrl'
-    });
-  }]);
+      $routeProvider.when('/hub', {
+        templateUrl: '../views/hub.html',
+        controller: 'HubCtrl'
+      });
+    }]);
 
 })();
